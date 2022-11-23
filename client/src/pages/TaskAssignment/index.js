8334dda0b8e164f9a19f63756548f86569dd2add
hieptth <hieptth71@gmail.com>
2022-11-23 20:24:58 +0700

    add ChooseRoute.js

--- client/src/pages/TaskAssignment/index.js
+++ client/src/pages/TaskAssignment/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import AreaMap from '../../components/AreaMap';
 import Calendar from '../../components/Calendar';
+import { mainTextColor, mainGreen } from '../../utils/constants';
+import Form from 'react-bootstrap/Form';
+import { useState, useEffect } from 'react';
+import MCPModal from '../../components/modals/MCPModal';
+import { useSelector } from 'react-redux';
 
 const TaskAssignment = () => {
-  return (
-    <div>
-      <Calendar />
-    </div>
-  );
 };
 
 export default TaskAssignment;
