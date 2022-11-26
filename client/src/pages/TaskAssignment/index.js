b4189e2e4ae76941b290b2989b0d152ea34d7c62
blackcat22521 <blackcat22521@gmail.com>
2022-11-26 16:37:29 +0700

    updateLoc
    component assignCollector

--- client/src/pages/TaskAssignment/index.js
+++ client/src/pages/TaskAssignment/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import AreaMap from '../../components/AreaMap';
 import Calendar from '../../components/Calendar';
+import CollectorTable from '../../components/CollectorTable/CollectorTable';
 
 const TaskAssignment = () => {
   return (
     <div>
       <Calendar />
+      <CollectorTable/>
     </div>
   );
 };
