5fe475acdfd418763b198f8bd206eef335350e90
hieptth <hieptth71@gmail.com>
2022-12-01 18:53:35 +0700

    resolve conflict

--- client/src/pages/TaskAssignment/index.js
+++ client/src/pages/TaskAssignment/index.js
@@ -8,35 +8,26 @@ import MCPModal from '../../components/modals/MCPModal';
 import { useSelector } from 'react-redux';
 import styles from "./Modal.module.css";
 import ChooseRouteModal from './ChooseRouteModal';
-<<<<<<< HEAD
-import CollectorTable from '../../components/CollectorTable/CollectorTable';
-
-=======
->>>>>>> 546e87314bb40dd4756b80e8f1842af29a3c5246
+import CollectorTable from '../../components/CollectorTable/CollectorTable'
 
 const TaskAssignment = () => {
 
     const [isOpen, setIsOpen] = useState(false);
-<<<<<<< HEAD
     const [opened, setOpened] = useState(false);
-    return (
-        <div>
-            <p>
-                <button className={styles.primaryBtn} onClick={() => setIsOpen(true)}> Open Modal </button>
-                {isOpen && <ChooseRouteModal setIsOpen={setIsOpen} />}
-            </p>
-            <p>
-                <button className={styles.primaryBtn} onClick={() => setOpened(true)}> Open Modal 2 </button>
-                {opened && < CollectorTable setOpened={setOpened} />}
-            </p>
-=======
 
     return (
-        <div>
-            <button className={styles.primaryBtn} onClick={() => setIsOpen(true)}> Open Modal </button>
-            {isOpen && <ChooseRouteModal setIsOpen={setIsOpen} />}
->>>>>>> 546e87314bb40dd4756b80e8f1842af29a3c5246
-        </div>
+        <>
+            <div>
+                <p>
+                    <button className={styles.primaryBtn} onClick={() => setIsOpen(true)}> Open Modal </button>
+                    {isOpen && <ChooseRouteModal setIsOpen={setIsOpen} />}
+                </p>
+                <p>
+                    <button className={styles.primaryBtn} onClick={() => setOpened(true)}> Open Modal 2 </button>
+                    {opened && < CollectorTable setOpened={setOpened} />}
+                </p>
+            </div>
+        </>
     )
 };
 
