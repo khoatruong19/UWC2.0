4599df21dae7221133f2761210fa2aa0dfeb39e6
nqhuy137 <nquochuy137@gmail.com>
2022-12-05 00:24:55 +0700

    adjust style of EmployeeList page

--- client/src/pages/EmployeeList/index.js
+++ client/src/pages/EmployeeList/index.js
@@ -1,8 +1,14 @@
-import { Paper, TableContainer, Table, TableHead, TableRow, TableCell, TableBody } from '@mui/material'
+import { Paper, TableContainer, Table, TableHead, TableRow, TableCell, TableBody, FormControl, InputLabel, Select, MenuItem } from '@mui/material'
 import React from 'react'
 import { employees } from './employees'
+import './style.css'
 
 function EmployeeList() {
+    const [employeeType, setEmployeeType] = React.useState('');
+
+    const handleChange = (event) => {
+        setEmployeeType(event.target.value);
+    };    
   return (
     <div 
         className="conntainer p-5"
@@ -13,7 +19,26 @@ function EmployeeList() {
         }}
     >  
         <h2 className='mb-4'>Employee List</h2>
-            
+        <div className="mb-2 dropdown">
+
+            <FormControl sx={{ m: 1, minWidth: 80 }}>
+                <InputLabel id="demo-simple-select-autowidth-label">Type</InputLabel>
+                <Select
+                labelId="demo-simple-select-autowidth-label"
+                id="demo-simple-select-autowidth"
+                value={employeeType}
+                onChange={handleChange}
+                autoWidth
+                label="Type"
+                >
+                <MenuItem value="">
+                    <em>None</em>
+                </MenuItem>
+                <MenuItem value={10}>Janitor</MenuItem>
+                <MenuItem value={21}>Collector</MenuItem>
+                </Select>
+            </FormControl>
+        </div>
         <TableContainer style={{
             backgroundColor: "#749F82",
             color: "#fff"
@@ -23,11 +48,12 @@ function EmployeeList() {
             <TableRow style={{
                 backgroundColor: "#A8E890",
             }}>
-                <TableCell style={{textColor: "#fff"}}>Employee ID</TableCell>
+                <TableCell>Employee ID</TableCell>
                 <TableCell align="center">Name</TableCell>
                 <TableCell align="center">Age</TableCell>
                 <TableCell align="center">Address</TableCell>
                 <TableCell align="center">Phone no</TableCell>
+                <TableCell align="center">Action</TableCell>
             </TableRow>
             </TableHead>
             <TableBody>
@@ -43,6 +69,16 @@ function EmployeeList() {
                     <TableCell align="center">{row.age}</TableCell>
                     <TableCell align="center">{row.address}</TableCell>
                     <TableCell align="center">{row.phonenumber}</TableCell>
+                    <TableCell align="center" style={{display: "flex"}}>
+                        <div className="icons-flex-container" style={{display: "flex", margin: "0 auto"}}>
+                            <div className="pencil-icon">
+                                <svg width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" fill-rule="evenodd" d="M3 18L15 6l3 3L6 21H3v-3ZM16 5l2-2l3 3l-2.001 2.001L16 5Z"/></svg>
+                            </div>
+                            <div className="trash-icon">
+                                <svg width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="M4 5h3V4a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v1h3a1 1 0 0 1 0 2h-1v13a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V7H4a1 1 0 1 1 0-2Zm3 2v13h10V7H7Zm2-2h6V4H9v1Zm0 4h2v9H9V9Zm4 0h2v9h-2V9Z"/></svg>
+                            </div>
+                        </div>
+                    </TableCell>
                 </TableRow>
             ))}
             </TableBody>
